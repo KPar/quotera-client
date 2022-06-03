d32b1eab130088ae218fd03d0aa801dd79f30cb4
Kenny P <kenparedes27@gmail.com>
2022-06-03 02:29:40 -0400

    Fixed case sensitivity in BookAdderSearchbar

--- src/components/CreateReflection/BookAdderSearchBar.jsx
+++ src/components/CreateReflection/BookAdderSearchBar.jsx
@@ -19,7 +19,7 @@ function BookAdderSearchBar() {
         return;
       }
       try{
-          let res = await fetch(`https://quotera.herokuapp.com/books/q?data=${inputRef.current.value.trim()}`);
+          let res = await fetch(`https://quotera.herokuapp.com/books/q?data=${inputRef.current.value.trim().toLowerCase()}`);
           if(res.status===404){
             setResults([])
           }else{
